d59f1fc62dc75d5b362c9b0a381a6fcfa05e3faf
Tobias Schmidt <tobias2.schmidt@ukr.de>
2025-06-11 15:33:26 +0200

    Rewrite test-data.R

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,56 +1,33 @@
-library(testthat)
+test_that("RP_Mod$Normal should be a strict subset of RP", {
+  mod_idx <- match(RP_Mod[[1]]$SMILES, RP$SMILES)
+  expect_false(any(is.na(mod_idx)))
+  RP_Sub <- RP[mod_idx, c("NAME", "SMILES", "RT")]
+  rownames(RP_Sub) <- NULL
+  expect_identical(RP_Sub, RP_Mod$Normal)
+  # Because RP_Mod$Normal only contains metabolites that are present in RP and
+  # we ensure that all datasets in RP_Mod have the same metabolites (next test),
+  # we also know that all other datasets in RP_Mod are subsets of RP.
+})
 
-opts <- options(testthat.show_trace = FALSE)
+test_that("All datasets in RP_Mod should have the same metabolites", {
+  RP_Mod_1 <- RP_Mod[[1]][, c("NAME", "SMILES")]
+  for (i in names(RP_Mod)) {
+    RP_Mod_i <- RP_Mod[[i]][, c("NAME", "SMILES")]
+    expect_identical(RP_Mod_i, RP_Mod_1)
+  }
+})
 
-data_funcs <- c(
-    # Full Datasets
-    "read_rp_xlsx",
-    "read_retip_hilic_data",
-    ".read_hilic_xlsx",
-    # ".read_rp_axmm_xlsx", # File missing. Fadi is looking for it.
-    # Smaller Datasets for Model Adjustment
-    ".read_rp_steep_xlsx",
-    ".read_rp_flat_xlsx",
-    ".read_rp_t25_xlsx",
-    ".read_rp_fr25_xlsx",
-    ".read_rp_t25_fr25_xlsx",
-    ".read_rp_t25_fr25_steep_xlsx",
-    # Validation Data Sets
-    ".read_rp_steep_val_xlsx",
-    ".read_rp_flat_val_xlsx",
-    ".read_rp_t25_val_xlsx",
-    ".read_rp_fr25_val_xlsx",
-    ".read_rp_t25_fr25_val_xlsx",
-    ".read_rp_t25_fr25_steep_val_xlsx",
-    # For Package Testing
-    "read_rpadj_xlsx",
-    # "read_rp_lasso_model_rds", # Model, not a dataset.
-    NULL
-)
-# (1) This excel file is missing. Fadi is looking for it.
+test_that("No metabolite of RP_Val[[i]] should be included in RP", {
+  expect_false(any(RP_Val[[1]]$Name %in% RP$Name))
+  expect_false(any(RP_Val[[1]]$SMILES %in% RP_Mod[[1]]$SMILES))
+  # It's sufficient to test this for the first dataset in RP_Val, because in the
+  # next test we ensure that all datasets in RP_Val have the same metabolites.
+})
 
-if (pkg_file("misc/datasets") == "") {
-    # 'misc/datasets' is not available, i.e., we are not in a
-    # development environment and we can not use/test our private
-    # functions depending on raw data files not shipped with the
-    # package (e.g. the source files for lazy loaded datasets).
-    # Therefore we filter out all functions starting with a dot.
-    data_funcs <- data_funcs[!startsWith(data_funcs, ".read_")]
-}
-
-test_that("read_xxx_works", {
-    # For every function we check wheter:
-    # 1. It can be called without throwing an error
-    # 2. It returns a data.frame object
-    # 3. The data.frame has at least columns NAME and SMILES
-    # 4. There should be now row with only NAs
-    lapply(data_funcs, function(fn) {
-        f <- get(fn)
-        df <- try(f(), silent = TRUE)
-        errmsg <- sprintf("Test for df <- %s() failed", fn)
-        expect_false(inherits(df, "try-error"), errmsg)
-        expect_true(is.data.frame(df), errmsg)
-        expect_true(all(c("NAME", "SMILES") %in% toupper(colnames(df))), errmsg)
-        expect_true(all(apply(df, 1, function(x) !all(is.na(x)))), errmsg)
-    })
+test_that("All datasets in RP_Val should have the same metabolites", {
+  RP_Val_1 <- RP_Val[[1]][, c("NAME", "SMILES")]
+  for (i in names(RP_Val)) {
+    RP_Val_i <- RP_Val[[i]][, c("NAME", "SMILES")]
+    expect_identical(RP_Val_i, RP_Val_1)
+  }
 })
