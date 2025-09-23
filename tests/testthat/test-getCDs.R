5ce0ef1a05f7a973c17fbe2119991a93c39013c5
Tobias Schmidt <tobias2.schmidt@ukr.de>
2025-09-23 08:39:39 +0200

    v1.2.1
    - Add updated Measurements `Measurements_v8.xlsx` to `inst/extdata/`. The new
      list contains corrections to the old `RP` dataset plus 1660 new measurements
      measured on a total of 18 different chromatographic environments.
    - Reintroduced RAM caching (although hugely simplified).

--- tests/testthat/test-getCDs.R
+++ tests/testthat/test-getCDs.R
@@ -5,6 +5,10 @@ test_that("getCDs works correctly", {
     # Helpers
     measure <- function(expr) { system.time(expr)[[3]] }
 
+    # Clear cached CDs
+    options(FastRet.cachedCDs = NULL)
+    expect_true(is.null(nrow(getOption("FastRet.cachedCDs"))))
+
     # Simple test case with known molecules from package data
     X <- RP[1:5, ]
     rt1 <- measure(Y1 <- getCDs(X, verbose = 0))
@@ -13,6 +17,7 @@ test_that("getCDs works correctly", {
     expect_true(all(colnames(Y1)[4:nc] %in% CDFeatures))
     expect_equal(Y1$Fsp3, c(0.75, 1/3, 0.00, 0.50, 0.50))
     expect_true(rt1 < 0.05)
+    expect_equal(nrow(getOption("FastRet.cachedCDs")), 1788)
 
     # Test with unknown SMILES (not in package data)
     smi <- c("CCC", "CCCC", "CCCCC", "CCC")
@@ -22,9 +27,11 @@ test_that("getCDs works correctly", {
     expect_equal(Y2$SMILES, Y2$SMILES)
     expect_equal(ncol(Y2), ncol(df) + length(CDFeatures))
     expect_true(rt2 > rt1)
+    expect_equal(nrow(getOption("FastRet.cachedCDs")), 1791)
 
     # Test with multiple workers
+    options(FastRet.cachedCDs = getOption("FastRet.cachedCDs")[1:1788, ]) # reset cache to original
     rt3 <- measure(Y3 <- getCDs(df, verbose = 0, nw = 2))
     expect_equal(Y3, Y2)
-    expect_true(rt3 > rt1) # should take more time due to parallel overhead
+    expect_true(rt3 > rt2) # should take more time due to parallel overhead
 })
